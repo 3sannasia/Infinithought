00fe524fd2ecbd3dac8ab8512c407001119cddc0
Akash Sannasi <54860072+3sannasia@users.noreply.github.com>
2021-11-16 15:46:01 -0600

    Add files via upload
    Updated Friend file with comments

--- Friend.py
+++ Friend.py
@@ -3,34 +3,30 @@ from typing import Dict
 from typing import List
 import math
 
+# Friend class represents edges between nodes(User Objects), to represent the friend relationship
 class Friend:
     def __init__(self, fid, u1:User, u2:User) -> None:
-        self._friendId = int(fid)
-        self._first = u1
-        self._second = u2
+        self._friendId = int(fid)                         # Initializes the friendID of the Friend
+        self._first = u1                                  # Initializes the source connection of the Friend edge to first user passed
+        self._second = u2                                 # Initializes the destination of the connection of the Friend edge to the second user passed
+        self.FindProximity()                              # NOT SURE WHAT THIS IS DOING (is it a dictionary of proximities?)
     
+    # Returns array of the first and second user that are connected by this Friend object edge
     @property
     def users(self)->List:
         return [self._first, self._second]
 
+    # Returns array of the first and second user that are connected by this Friend object edge    
     @property
     def friendID(self)->int:
         return self._friendId
 
+    # Returns proximity of this Friend relationship between two nodes in the social network
     @property
     def proximity(self)->Dict:
         return self._prox
 
-    def AddProximity(self, movies, music, sports, food, travel, default) -> None:
-        self._prox = {
-            "Movies" : movies,
-            "Music" : music,
-            "Sports" : sports,
-            "Food" : food,
-            "Travel" : travel,
-            "Default" : default
-        }
-
+    # Void function that finds proximity of each dictionary of opinions for each category in the social network nodes
     def FindProximity(self)->None:
         self._prox = {
             "Movies" : self._GetProx("Movies"),
@@ -41,16 +37,20 @@ class Friend:
             "Default" : self._GetDefProx()
         }
 
+    # Calculates the proximity of the specific category passed into it
     def _GetProx(self, category)->float:
+        # SHOULD WE CHECK IF CATEGORY IS VALID?
+        default_proximity = 11.0
         u1_items = self._first.interests.get(category).keys()
         u2_items = self._second.interests.get(category).keys()
         overlap_items = [value for value in u1_items if value in u2_items]
         
-        if (len(overlap_items) == 0):
-            return 11.0
+        if (len(overlap_items) == 0):     #if no overlap in items between User 1 and User 2 in the friend relationship, default proximity assigned
+            return default_proximity
 
         total = 0
         
+        # Calculates proximity by taking into account the items shared by User 1 and User 2
         for item in overlap_items:
             a = sum(self._first.interests.get(category).get(str(item)))
             b = sum(self._second.interests.get(category).get(str(item)))
@@ -58,8 +58,10 @@ class Friend:
         total /= len(overlap_items)
         return math.sqrt(total)
 
+    # Gets the definitive total proximity of all the categories within the interests of User 1 and User 2 collectively into one proximity value
+    # Returns that definitive total proximity in the friend relationship
     def _GetDefProx(self)->float:
-        categories = ["Movies", "Music", "Sports", "Food", "Travel"]
+        categories = ["Movies", "Music", "Sports", "Food", "Travel"] 
         total = 0
         valid = 0
         for cat in categories:
@@ -90,6 +92,3 @@ class Friend:
 # print(f.proximity["Movies"])
 # print(f.proximity["Default"])
 
-
-
-    
