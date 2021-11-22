0633aef4477d45774070c3fc8403ddf4abaae56a
Akash Sannasi <54860072+3sannasia@users.noreply.github.com>
2021-11-22 00:17:05 -0600

    Add files via upload

--- FriendTestMethods.py
+++ FriendTestMethods.py
@@ -7,18 +7,36 @@ class FriendTestMethods(unittest.TestCase):
     user2 = User(234)
     friend_relationship1 = Friend(123, user1, user2)
     opinion = Opinion("Movies", "Frozen", 10)
-    o1 = Opinion("Movies", "A", 3)
-    o2 = Opinion("Movies", "B", 3)
-    o2k = Opinion("Movies", "B", 10)
-    o3 = Opinion("Movies", "C", 4)
-    o4 = Opinion("Movies", "D", 4)
-    user1.AddOpinion(o1)
-    user1.AddOpinion(o2)
-    user1.AddOpinion(o3)
-    user2.AddOpinion(o2k)
-    user2.AddOpinion(o3)
-    user2.AddOpinion(o4)
+    opinion1 = Opinion("Movies", "A", 3)
+    opinion2 = Opinion("Movies", "B", 3)
+    opinion3 = Opinion("Movies", "Z" , 10)
+    opinion4 = Opinion("Movies", "C", 4)
+    opinion5 = Opinion("Movies", "D", 4)
+    user1.AddOpinion(opinion)
+    user1.AddOpinion(opinion1)
+    user1.AddOpinion(opinion2)
+    user2.AddOpinion(opinion3)
+    user2.AddOpinion(opinion4)
+    user2.AddOpinion(opinion5) 
     friend_relationship1.proximity["Movies"]
+
+    u1 = User(123)
+    u2 = User(234)
+    o1 = Opinion("Sports", "A", 3)
+    o2 = Opinion("Sports", "B", 5)
+    o2k = Opinion("Sports", "B", 5)
+    o3 = Opinion("Sports", "C", 4)
+    o4 = Opinion("Sports", "D", 4)
+    u1.AddOpinion(o1)
+    u1.AddOpinion(o2)
+    u1.AddOpinion(o3)
+    u2.AddOpinion(o2k)
+    u2.AddOpinion(o3)
+    u2.AddOpinion(o4)
+    friend_relationship2 = Friend(123, u1, u2)
+    print(friend_relationship2.proximity["Sports"])
+
+
 
 
     def test_data_members(self):
@@ -27,8 +45,10 @@ class FriendTestMethods(unittest.TestCase):
         self.assertEqual(self._friendId, 123)
         
     def test_find_proximity(self):
-        self.assertEqual(friend_relationship1.proximity["Movies"], FIND VAL AFTER PRINTING)
+        self.assertEqual(friend_relationship1.proximity["Movies"], 4.949747468305833)
+        self.assertEqual(friend_relationship1.proximity["Default"], 4.949747468305833)
         self.assertEqual(friend_relationship1.proximity["Default"], 11.0)
+        self.assertEqual(friend_relationship2.proximity["Sports"], 0.0)
 
 #TO BE FINISHED 
 
@@ -49,4 +69,4 @@ if __name__ == '__main__':
 # u1.AddOpinion(o3)
 # u2.AddOpinion(o2k)
 # u2.AddOpinion(o3)
-# u2.AddOpinion(o4)
+# u2.AddOpinion(o4)68
