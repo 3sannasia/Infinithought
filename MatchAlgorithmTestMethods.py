7ef2afcd8569f519556ec01f287c1b058d6e3bd9
Akash Sannasi <54860072+3sannasia@users.noreply.github.com>
2021-11-29 12:45:58 -0600

    Add files via upload

--- MatchAlgorithmTestMethods.py
+++ MatchAlgorithmTestMethods.py
@@ -1,3 +1,28 @@
 import unittest
 
 class MatchAlgorithmTestMethods(unittest.TestCase):
+    def TestLength(self):
+        user1 = User(1)
+        user2 = User(2)
+        network = Network()
+        self.assertEqual(self.Length(network, user1, user2, "Movies"), 12.0)
+        
+
+    def TestGetDijkstraList(self):
+
+
+    def TestPotentialMatches(self):
+
+    def TestGetWeightedDictionary(self):
+
+    def GetDictionarySum(self):
+
+    def GetBestMatch(self):
+
+
+
+
+    
+
+
+
