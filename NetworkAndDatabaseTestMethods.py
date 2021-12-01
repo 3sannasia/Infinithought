681ec0827fdaea6831bf9b32cebcd8d5d849b276
Akash Sannasi <54860072+3sannasia@users.noreply.github.com>
2021-11-30 20:53:12 -0600

    Add files via upload

--- NetworkAndDatabaseTestMethods.py
+++ NetworkAndDatabaseTestMethods.py
@@ -1,182 +1,279 @@
 from Friend import Friend
 from User import Opinion, User
 import database
-import Network
+from Network import Network
 
-
-import matplotlib.pyplot as plt
 import unittest
 
+
 class NetworkAndDatabaseTestMethods(unittest.TestCase):
-    # Network class utilizes functions within the Database class
-    def AddUser(self):
-        network = Network()
+
+    def test_add_user(self):
         logininfo1 = "password1"
         logininfo2 = "password2"
         opinion1= Opinion("Movies", "Matilda", 2)
         opinion2 = Opinion("Movies", "The Kissing Booth", 2)
         opinion3 = Opinion("Movies", "Avatar", 10)
         listOfOpinions = [opinion1, opinion2, opinion3]
-        network.AddUser(logininfo1, listOfOpinions)
+        database.make_tables()
+        network = Network()
+        network.AddUser(logininfo1, [])
         self.assertEqual(int(database.get_available_userid()), 2)
         listOfOpinions = [opinion1, opinion2, opinion3]
-        network.AddUser(logininfo2, listOfOpinions)
+        network.AddUser(logininfo2, [])
         self.assertEqual(int(database.get_available_userid()), 3)
-    
-    def DeleteUser(self):
-        network = Network()
+        self.assertEqual(len(database.get_all_users()), 2)
+        database.delete_all_tables()
+
+    def test_DeleteUser(self):
         logininfo1 = "password1"
         logininfo2 = "password2"
         opinion1= Opinion("Movies", "Matilda", 2)
         opinion2 = Opinion("Movies", "The Kissing Booth", 2)
         opinion3 = Opinion("Movies", "Avatar", 10)
         listOfOpinions = [opinion1, opinion2, opinion3]
+        database.make_tables()
+        network = Network()
         network.AddUser(logininfo1, listOfOpinions)
         listOfOpinions = [opinion1, opinion2, opinion3]
         network.AddUser(logininfo2, listOfOpinions)
         network.DeleteUser(2)
         self.assertEqual(int(database.get_available_userid()), 2)
+        database.delete_all_tables()
 
-    def DatabaseAddOpinion(self):
-        network = Network()
+    def test_DatabaseAddOpinion(self):
         logininfo1 = "password1"
         logininfo2 = "password2"
         opinion1= Opinion("Movies", "Matilda", 2)
         opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
         opinion3 = Opinion("Movies", "Avatar", 10)
         listOfOpinions = [opinion1, opinion2, opinion3]
+        database.make_tables()
+        network = Network()
         network.AddUser(logininfo1, listOfOpinions)
         listOfOpinions = [opinion1, opinion2, opinion3]
         network.AddUser(logininfo2, listOfOpinions)
         opinion_to_add = Opinion("Movies", "Frozen", 10)
-        database.add_opinion_to_opinions(2, opinion_to_add)
-        self.assertEqual(database.get_opinions_for_user(2), [opinion1, opinion2, opinion3, opinion_to_add])
+        database.add_opinion_to_opinions(1, opinion_to_add)
+        counter = 0
+        for opinions in database.get_opinions_for_user(2):
+            counter = counter + 1
+            self.assertEqual(opinions.category, "Movies")
+            if counter == 1:
+                self.assertEqual(opinions.item, "Matilda")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 2:
+                self.assertEqual(opinions.item, "The Kissing Booth")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 3:
+                self.assertEqual(opinions.item, "Avatar")
+                self.assertEqual(opinions.rating, 10)
+            elif counter == 4:
+                self.assertEqual(opinions.item, "Frozen")
+                self.assertEqual(opinions.rating, 10)
+        database.delete_all_tables()
+
+
     
-    def DatabaseRemoveOpinion(self):
-        network = Network()
+    def test_DatabaseRemoveOpinion(self):
         logininfo1 = "password1"
         logininfo2 = "password2"
         opinion1= Opinion("Movies", "Matilda", 2)
         opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
         opinion3 = Opinion("Movies", "Avatar", 10)
         listOfOpinions = [opinion1, opinion2, opinion3]
+        database.make_tables()
+        network = Network()
         network.AddUser(logininfo1, listOfOpinions)
-        listOfOpinions = [opinion1, opinion2, opinion3]
-        network.AddUser(logininfo2, listOfOpinions)
         opinion_to_add = Opinion("Movies", "Frozen", 10)
-        database.add_opinion_to_opinions(2, opinion_to_add)
-        self.assertEqual(database.get_opinions_for_user(2), [opinion1, opinion2, opinion3, opinion_to_add])
-        database.delete_opinion_from_opinions_for_user(opinion_to_add, 2)
-        self.assertEqual(database.get_opinions_for_user(2), listOfOpinions)
-
-    def NetworkAddOpinion(self):
-        first_user = User(123)
-        network = Network()
-        opinion1= Opinion("Movies", "Matilda", 2)
-        opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
-        opinion3 = Opinion("Movies", "Avatar", 10)
-        network.AddOpinion(first_user, opinion1)
-        network.AddOpinion(first_user, opinion2)
-        network.AddOpinion(first_user, opinion3)
-        self.assertEqual(database.get_opinions_for_user(123), [opinion1, opinion2, opinion3])    
-    
-    def get_available_userid(self):
-        self.assertEqual(database.get_available_userid(), 1)
-        user1 = User(2)
-        self.assertEqual(database.get_available_userid(), 3)
-
-    def get_available_friend_id(self):
-        user1 = User(1)
-        user2 = User(2)
-        self.assertEqual(database.get_available_friend_id(), 1)
-        friend_relationship1 = Friend(1, user1, user2)
-        self.assertEqual(database.get_available_friend_id(), 3)
-    
-    def get_all_users(self):
-        network = Network()
-        self.assertEqual(database.get_all_users, [])
-        logininfo1 = "password1"
-        logininfo2 = "password2"
-        opinion1= Opinion("Movies", "Matilda", 2)
-        opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
-        opinion3 = Opinion("Movies", "Avatar", 10)
-        listOfOpinions = [opinion1, opinion2, opinion3]
-        network.AddUser(logininfo1, listOfOpinions)
-        listOfOpinions = [opinion1, opinion2, opinion3]
-        network.AddUser(logininfo2, listOfOpinions)
-        # self.assertEqual(database.get_all_users, [])
-
-    def add_users_to_users(self):
-        user1 = User(1)
-        database.add_user_to_users(user1, "password")
-        self.assertEqual(database.get_available_userid, 2)
-
-    def delete_user_from_users(self):
-        user1 = User(1)
-        database.add_user_to_users(user1, "password")
-        self.assertEqual(database.get_available_userid, 2)
-        database.delete_user_from_users(1)
-        self.assertEqual(database.get_available_userid, 1)
-
-    def add_friendship_to_friends(self):
-        user1 = User(1)
-        user2 = User(2)
-        friend_relationship1 = Friend(1, user1, user2)
-        database.add_friendship_to_friends(friend_relationship1)
-        self.assertEqual(database.get_available_friend_id, 2)
-
-    def delete_friendship_from_friends(self):
-        user1 = User(1)
-        user2 = User(2)
-        friend_relationship1 = Friend(1, user1, user2)
-        database.add_friendship_to_friends(friend_relationship1)
-        self.assertEqual(database.get_available_friend_id, 2)
-        database.delete_friendship_to_friends(1)
-        self.assertEqual(database.get_available_friend_id, 1)
-
-    def delete_friendship_only_userids(self):
-        user1 = User(1)
-        user2 = User(2)
-        friend_relationship1 = Friend(1, user1, user2)
-        database.add_friendship_to_friends(friend_relationship1)
-        database.delete_friendship_only_userids(user1, user2)
-        self.assertEqual(database.get_available_friend_id, 1)
-
-    def delete_friendship_only_one_userid(self):
-        user1 = User(1)
-        user2 = User(2)
-        friend_relationship1 = Friend(1, user1, user2)
-        database.add_friendship_to_friends(friend_relationship1)
-        database.delete_friendship_only_userids(user1, user2)
-        self.assertEqual(database.get_available_friend_id, 1)
-    
-    def get_all_friends(self):
-        user1 = User(1)
-        user2 = User(2)
-        friend_relationship1 = Friend(1, user1, user2)
-        database.add_friendship_to_friends(friend_relationship1)
-        user3 = User(3)
-        user4 = User(4)
-        friend_relationship2 = Friend(2, user3, user4)
-        database.add_friendship_to_friends(friend_relationship2)
-        self.assertEqual(database.get_all_friends(), [friend_relationship1, friend_relationship2])
-    
-    def delete_opinion_from_opinions_for_user(self):
-        first_user = User(1)
-        opinion = Opinion("Movies", "Frozen", 10)
-        first_user.AddOpinion(opinion)
-        database.add_user_to_users(first_user, "password")
-        database.delete_opinion_from_opinions_for_user(opinion, 123)
-        self.assertEqual(database.get_available_userid(1), 1)
+        listOfOpinions2 = [opinion1, opinion2, opinion3, opinion_to_add]
+        network.AddUser(logininfo2, listOfOpinions2)
+        counter = 0
+        for opinions in database.get_opinions_for_user(2):
+            counter = counter + 1
+            self.assertEqual(opinions.category, "Movies")
+            if counter == 1:
+                self.assertEqual(opinions.item, "Matilda")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 2:
+                self.assertEqual(opinions.item, "The Kissing Booth")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 3:
+                self.assertEqual(opinions.item, "Avatar")
+                self.assertEqual(opinions.rating, 10)
+            elif counter == 4:
+                self.assertEqual(opinions.item, "Frozen")
+                self.assertEqual(opinions.rating, 10)
+        database.delete_all_tables()
         
-    def get_opinions_for_user(self):
+    def test_NetworkAddOpinion(self):
+        database.make_tables()
+        network = Network()
+        network.AddUser("password", [])
+        opinion1= Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
+        opinion3 = Opinion("Movies", "Avatar", 10)
+
+        network.AddUser("password2", [opinion1, opinion2])
+        network.MakeFriend(network.GetUser(1), network.GetUser(2))
+        network.AddOpinion(network.GetUser(2), opinion3)
+        counter = 0
+        for opinions in database.get_opinions_for_user(2):
+            counter = counter + 1
+            self.assertEqual(opinions.category, "Movies")
+            if counter == 1:
+                self.assertEqual(opinions.item, "Matilda")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 2:
+                self.assertEqual(opinions.item, "The Kissing Booth")
+                self.assertEqual(opinions.rating, 2)
+            elif counter == 3:
+                self.assertEqual(opinions.item, "Avatar")
+                self.assertEqual(opinions.rating, 10)
+        database.delete_all_tables()
+
+    
+    def test_get_available_userid(self):
+        opinion1 = Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "Frozen", 2)
+        database.make_tables()
+        network = Network()
+        self.assertEqual(database.get_available_userid(), 1)
+        network.AddUser("password1", [opinion1, opinion2])
+        self.assertEqual(database.get_available_userid(), 2)
+        network.AddUser("password2", [opinion1, opinion2])
+        self.assertEqual(database.get_available_userid(), 3)
+        database.delete_all_tables()
+
+
+    def test_get_available_friend_id_and_make_friend(self):
+        database.make_tables()
+        network = Network()
+        self.assertEqual(database.get_available_friend_id(), 1)
+        opinion1 = Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "Frozen", 2)
+        self.assertEqual(database.get_available_userid(), 1)
+        network.AddUser("password1", [opinion1, opinion2])
+        self.assertEqual(database.get_available_userid(), 2) #Not one because when a user is added, a friend connection is automatically made
+        network.AddUser("password2", [opinion1, opinion2])
+        self.assertEqual(database.get_available_friend_id(), 2)
+        network.MakeFriend(network.GetUser(1), network.GetUser(2))
+        self.assertEqual(database.get_available_friend_id(), 3)
+        database.delete_all_tables()
+
+    def test_get_all_users(self): 
+        database.make_tables()
+        network = Network()
+        logininfo1 = "password1"
+        logininfo2 = "password1"
+        self.assertEqual(database.get_all_users(), [])
+        opinion1= Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "The Kissing Booth", 2) 
+        opinion3 = Opinion("Movies", "Avatar", 10)
+        listOfOpinions = [opinion1, opinion2, opinion3]
+        network.AddUser(logininfo1, listOfOpinions)
+        listOfOpinions = [opinion1, opinion2, opinion3]
+        network.AddUser(logininfo2, listOfOpinions)
+        self.assertEqual(len(database.get_all_users()), 2)
+        database.delete_all_tables()
+
+    def test_add_users_to_users(self):
+        user1 = User(1);
+        database.make_tables()
+        opinion1 = Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "The Kissing Booth", 2)
+        opinion3 = Opinion("Movies", "Avatar", 10)
+        listOfOpinions = [opinion1, opinion2, opinion3]
+        network = Network()
+        network.AddUser("password", listOfOpinions)
+        database.add_user_to_users(user1, "password")
+        self.assertEqual(database.get_available_userid(), 2)
+        database.delete_all_tables()
+
+    def test_delete_user_from_users(self):
+        database.make_tables()
+        user1 = User(1)
+        database.add_user_to_users(user1, "password")
+        self.assertEqual(database.get_available_userid(), 2)
+        database.delete_user_from_users(1)
+        self.assertEqual(database.get_available_userid(), 1)
+        database.delete_all_tables()
+
+    def test_add_friendship_to_friends(self):
+        database.make_tables()
+        user1 = User(1)
+        user2 = User(2)
+        friend_relationship1 = Friend(1, user1, user2)
+        database.add_friendship_to_friends(friend_relationship1)
+        self.assertEqual(database.get_available_friend_id(), 2)
+        database.delete_all_tables()
+
+    def test_delete_friendship_from_friends_and_UnFriend(self):
+        database.make_tables()
+        user1 = User(1)
+        user2 = User(2)
+        network = Network()
+        #friend relationship automatically made then below another added
+        friend_relationship1 = Friend(1, user1, user2)
+        database.add_friendship_to_friends(friend_relationship1)
+        self.assertEqual(database.get_available_friend_id(), 2)
+        network.MakeFriend(user1, user2)
+        self.assertEqual(database.get_available_friend_id(), 3)
+        network.UnFriend(user1, user2); #1 deleted
+        self.assertEqual(database.get_available_friend_id(), 1)
+        database.delete_all_tables()
+    
+    def test_get_all_friends(self):      
+        database.make_tables()
+        network = Network()
+        logininfo1 = "password1"
+        logininfo2 = "password2"
+        opinion1 = Opinion("Movies", "Matilda", 2)
+        opinion2 = Opinion("Movies", "The Kissing Booth", 2)
+        opinion3 = Opinion("Movies", "Avatar", 10)
+        listOfOpinions = [opinion1, opinion2, opinion3]
+        network.AddUser(logininfo1, listOfOpinions)
+        network.AddUser(logininfo2, listOfOpinions)
+        self.assertEqual(len(database.get_all_friends()), 1)
+        network.AddUser("password3", listOfOpinions)
+        self.assertEqual(len(database.get_all_friends()), 2)
+        network.AddUser("password4", listOfOpinions)
+        self.assertEqual(len(database.get_all_friends()), 3)
+        database.delete_all_tables()
+
+    def test_delete_opinion_from_opinions_for_user(self):
+        database.make_tables()
+        network = Network()
+        opinion = Opinion("Movies", "Frozen", 10)
+        opinion2 = Opinion("Movies", "Banana", 10)
+        opinion_list = [opinion, opinion2]
+        network.AddUser("password", opinion_list)
+        database.delete_opinion_from_opinions_for_user(opinion, 1)
+        updated_opinions_list = database.get_opinions_for_user(1)
+        self.assertEqual(len(updated_opinions_list), 1)
+        database.delete_all_tables()
+        
+    def test_get_opinions_for_user(self):
+        # database.delete_all_tables()
+        # return
+        database.make_tables()
+        self.assertEqual(len(database.get_opinions_for_user(1)), 0)
         first_user = User(1)
         opinion = Opinion("Movies", "Frozen", 10)
         first_user.AddOpinion(opinion)
+        network = Network()
+        network.AddUser("password", [opinion])
+        network.AddUser("password1", [opinion])
         database.add_user_to_users(first_user, "password")
-        self.assertEqual(database.get_opinions_for_user(1), 1)
+        self.assertEqual(len(database.get_opinions_for_user(1)), 1 )
+        opinions_list = database.get_opinions_for_user(1)
+        self.assertEqual(opinions_list[0].category, "Movies")
+        self.assertEqual(opinions_list[0].item, "Frozen")
+        self.assertEqual(opinions_list[0].rating, 10)
+        database.delete_all_tables()
 
-    def get_all_opinions(self):
+    def test_get_all_opinions(self):
+        database.make_tables()
         o1 = Opinion("Movies", "Lion King", 2)
         o2 = Opinion("Movies", "Lion King", 6)
         o3 = Opinion("Movies", "Lion King", 10)
@@ -188,6 +285,8 @@ class NetworkAndDatabaseTestMethods(unittest.TestCase):
         network = Network()
         network.AddUser("password1", [o1, o2, o3])
         network.AddUser("password2", [o4, o5, o6])
+        database.delete_all_tables()
+
 
 if __name__ == '__main__':
     unittest.main()
